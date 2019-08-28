dd20fe64364ab8c1f3c161149b983366de915a15
chuiyangliu <641322167@qq.com>
2019-08-29 01:12:56 +0800

    打war包版本

--- src/main/java/com/willow/advancednc/AdvancedncApplication.java
+++ src/main/java/com/willow/advancednc/AdvancedncApplication.java
@@ -2,9 +2,15 @@ package com.willow.advancednc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdvancedncApplication {
+public class AdvancedncApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AdvancedncApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedncApplication.class, args);
